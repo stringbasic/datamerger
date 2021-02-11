9737d2a04fe7e141489a2a0ba5f63e9fb18dee66
Nelson Ruivo <nelsonruivo@gmail.com>
2021-02-11 22:21:15 +0000

    Implemented initial options.

--- src/datamerger.cpp
+++ src/datamerger.cpp
@@ -7,12 +7,16 @@ using namespace std;
 using namespace datamerger;
 using namespace csv;
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   DataMergerApp app;
   app.mergeIt();
 
   cxxopts::Options options("datamerger", "Merge data");
-  options.add_options()("h,help", "print this help");
+  options.add_options()("h,help", "print this help")(
+      "main", "main file", cxxopts::value<std::string>())(
+      "references", "mapping files",
+      cxxopts::value<std::vector<std::string>>());
+  options.parse_positional({"main", "references"});
 
   if (argc == 1) {
     cout << options.help({""}) << endl;
@@ -23,6 +27,17 @@ int main(int argc, char *argv[]) {
   if (result.count("help")) {
     cout << options.help({""}) << endl;
     exit(0);
+  }
+
+  if (result.count("main")) {
+    cout << "main: " << result["main"].as<std::string>() << endl;
+  }
+
+  if (result.count("references")) {
+    for (auto& reference :
+         result["references"].as<std::vector<std::string>>()) {
+      cout << "reference: " << reference << endl;
+    }
   }
 
   CSVRow row;
