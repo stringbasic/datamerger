379a2a02787bbdf5576414b2151d207f75f0359b
Nelson Ruivo <nelsonruivo@gmail.com>
2021-02-12 23:13:54 +0000

    Added DelimWriter to build the output.

--- src/DataMergerApp.cpp
+++ src/DataMergerApp.cpp
@@ -24,50 +24,56 @@ DataMergerApp::~DataMergerApp() {
 bool DataMergerApp::processMap(string mapFile) {
   if (mapFile == "mapFile") return true;
   CSVReader mainReader(this->mainFile);
+  DelimWriter<ostream, ';', '"'> output(cout);
 
   int mainMappedColumn = -1;
   int mapMappedColumn = -1;
+  list<string> allColumns;
 
   int i = 0, j = 0;
   int lastIndex = mainReader.get_col_names().size() - 1;
   for (auto& mainC : mainReader.get_col_names()) {
     CSVReader mapReader(mapFile);
-    cout << mainC << ",";
+    allColumns.push_back(mainC);
     j = 0;
     for (auto& mapC : mapReader.get_col_names()) {
       if (mainC == mapC) {
         mainMappedColumn = i;
         mapMappedColumn = j;
       } else if (lastIndex == i) {
-        cout << mapC << ",";
+        allColumns.push_back(mapC);
       }
       j++;
     }
     i++;
   }
-  cout << endl;
+
+  output << allColumns;
 
   for (CSVRow& mainRow : mainReader) {
     CSVReader mapReader(mapFile);
+    list<string> allFields;
     int columnIndex = 0;
     for (CSVField& field : mainRow) {
       if (mainMappedColumn == columnIndex) {
-        cout << field.get<>() << ",";
+        allFields.push_back(field.get<string>());
         for (auto& mapRow : mapReader) {
           if (mapRow[mapMappedColumn].get<string>() == field.get<>()) {
             int mapIndex = 0;
             for (auto& mapField : mapRow) {
-              if (mapIndex != mapMappedColumn) cout << mapField.get<>() << ",";
+              if (mapIndex != mapMappedColumn) {
+                allFields.push_back(mapField.get<string>());
+              }
               mapIndex++;
             }
           }
         }
       } else {
-        cout << field.get<>() << ",";
+        allFields.push_back(field.get<string>());
       }
       columnIndex++;
     }
-    cout << endl;
+    output << allFields;
   }
 
   return true;
