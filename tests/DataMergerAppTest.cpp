8c5977ffea97018d7bce4c043df28f3b3356d48d
Nelson Ruivo <nelsonruivo@gmail.com>
2021-03-10 22:18:14 +0000

    Added test for multiple reference maps.

--- tests/DataMergerAppTest.cpp
+++ tests/DataMergerAppTest.cpp
@@ -8,12 +8,24 @@
 using namespace std;
 using namespace datamerger;
 
-TEST(Numbers, DataMergerAppOk) {
+TEST(DataMergerApp, MergeOneMap) {
   ostringstream output;
   DataMergerApp app("../../tests/test1-input1.csv", output);
   EXPECT_EQ(true, app.processHeader("../../tests/test1-input2.csv"));
   app.generateOutput();
-  ifstream expectedFile("../../tests/test1-output.csv", std::ios::in);
+  ifstream expectedFile("../../tests/test1-output.csv", ios::in);
+  stringstream expectedBuffer;
+  expectedBuffer << expectedFile.rdbuf();
+  EXPECT_EQ(expectedBuffer.str(), output.str());
+}
+
+TEST(DataMergerApp, MergeTwoMaps) {
+  ostringstream output;
+  DataMergerApp app("../../tests/test2-input1.csv", output);
+  EXPECT_EQ(true, app.processHeader("../../tests/test2-input2.csv"));
+  EXPECT_EQ(true, app.processHeader("../../tests/test2-input3.csv"));
+  app.generateOutput();
+  ifstream expectedFile("../../tests/test2-output.csv", ios::in);
   stringstream expectedBuffer;
   expectedBuffer << expectedFile.rdbuf();
   EXPECT_EQ(expectedBuffer.str(), output.str());
